2d798b85809e09d4b07916cbc0ebb67ea9bb491a
Andre Weissflog <floooh@gmail.com>
2023-11-14 18:31:16 +0100

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
 import * as vscode from 'vscode';
 import { start, assemble } from './kcide';
 
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate(ext: vscode.ExtensionContext) {
 
 	console.log('vscode-kcide: activate() called');
 	try {
-		const state = await start(context);
+		const ctx = await start(ext);
 		console.log('vscode-kcide: started');
 
-		context.subscriptions.push(vscode.commands.registerCommand('floooh.kcide.assemble', async () => {
-			await assemble(context, state);
+		ext.subscriptions.push(vscode.commands.registerCommand('floooh.kcide.assemble', async () => {
+			await assemble(ext, ctx);
 		}));
 	} catch (err) {
 		vscode.window.showErrorMessage(`Failed to setup KCIDE extension (${(err as Error).message}`);
--- src/kcide.ts
+++ src/kcide.ts
@@ -13,60 +13,214 @@ import {
 import {
     RootFileSystem,
     Wasm,
-    WasmPseudoterminal
 } from '@vscode/wasm-wasi';
+import fs from 'fs';
+import path from 'path';
 
-export interface State {
+export interface Context {
+    projectUri: Uri,
     wasm: Wasm,
     fs: RootFileSystem,
-    pty: WasmPseudoterminal,
-    terminal: Terminal, 
     diagnostics: DiagnosticCollection,
-    module: WebAssembly.Module,
+    asmx: WebAssembly.Module,
 };
 
-async function findMainSource(): Promise<string | undefined> {
-    const uris = await workspace.findFiles('src/main.asm', null, 1);
-    return (uris.length === 1) ? uris[0].fsPath : undefined;
+enum CPU { Z80 = 'Z80', M6502 = '6502' };
+enum System { KC852 = 'KC85/2', KC853 = 'KC85/3', KC854 = 'KC85/4' };
+enum OutputFileType { KCC = 'KCC' };
+
+// keep in sync with kcide.project.schema.json
+interface Project {
+    mainFile: string,
+    cpu: CPU,
+    system: System,
+    output: {
+        dir: string,
+        type: OutputFileType,
+        basename: string,
+    }
+};
+
+interface RunResult {
+    exitCode: number,
+    stdout?: string,
+    stderr?: string,
+};
+
+const projectDefaults: Project = {
+    mainFile: 'src/main.asm',
+    cpu: CPU.Z80,
+    system: System.KC854,
+    output: {
+        dir: 'build',
+        type: OutputFileType.KCC,
+        basename: 'out',
+    },
+};
+
+function isValidString(val: unknown): val is string {
+    if (typeof val !== 'string') {
+        return false;
+    }
+    if (val === '') {
+        return false;
+    }
+    return true;
 }
 
-export async function start(context: ExtensionContext): Promise<State> {
+function isValidCpu(val: unknown): val is CPU {
+    return isValidString(val) && (val in CPU);
+}
+
+function isValidSystem(val: unknown): val is System {
+    return isValidString(val) && (val in System);
+}
+
+function isValidOutputFileType(val: unknown): val is OutputFileType {
+    return isValidString(val) && (val in OutputFileType);
+}
+
+async function uriToWasmPath(ctx: Context, uri: Uri): Promise<string> {
+    const uriPath = await ctx.fs.toWasm(uri);
+    if (uriPath === undefined) {
+        throw new Error(`uriToWasmPath: ctx.fs.toWasm(${uriPath}) failed!`);
+    }
+    return uriPath;
+}
+
+function loadProject(ctx: Context): Project {
+    const projectJsonPath = path.join(ctx.projectUri.fsPath, 'kcide.project.json');
+    if (!fs.statSync(projectJsonPath).isFile()) {
+        return projectDefaults;
+    }
+    const projectJsonContent = fs.readFileSync(projectJsonPath, { encoding: 'utf8' });
+    const anyProject = JSON.parse(projectJsonContent);
+    return {
+        mainFile: isValidString(anyProject.mainFile) ? anyProject.mainFile : projectDefaults.mainFile,
+        cpu: isValidCpu(anyProject.cpu) ? anyProject.cpu : projectDefaults.cpu,
+        system: isValidSystem(anyProject.system) ? anyProject.system : projectDefaults.system,
+        output: {
+            dir: isValidString(anyProject.output?.dir) ? anyProject.output.dir : projectDefaults.output.dir,
+            type: isValidOutputFileType(anyProject.output?.type) ? anyProject.output.type : projectDefaults.output.type,
+            basename: isValidString(anyProject.output?.basename) ? anyProject.output.basename : projectDefaults.output.basename
+        }
+    };
+}
+
+function getMainSourceUri(ctx: Context, project: Project): Uri | undefined {
+    const filePath = path.join(ctx.projectUri.fsPath, project.mainFile);
+    const stat = fs.statSync(filePath, { throwIfNoEntry: false });
+    if ((stat !== undefined) && stat.isFile()) {
+        return Uri.file(filePath);
+    } else {
+        return undefined;
+    }
+}
+
+function ensureBuildDir(ctx: Context, project: Project): Uri {
+    const dirPath = path.join(ctx.projectUri.fsPath, project.output.dir);
+    const stat = fs.statSync(dirPath, { throwIfNoEntry: false });
+    if ((stat === undefined) || (!stat.isDirectory())) {
+        fs.mkdirSync(dirPath, { recursive: true });
+    }
+    return Uri.file(dirPath);
+}
+
+function getOutputObjectFileUri(ctx: Context, project: Project): Uri {
+    return Uri.file(path.join(ensureBuildDir(ctx, project).fsPath, `${project.output.basename}.hex`));
+}
+
+function getOutputListFileUri(ctx: Context, project: Project): Uri {
+    return Uri.file(path.join(ensureBuildDir(ctx, project).fsPath, `${project.output.basename}.lst`));
+}
+
+function getOutputFileUri(ctx: Context, project: Project, filename: string): Uri {
+    return Uri.file(path.join(ensureBuildDir(ctx, project).fsPath, filename));
+}
+
+export async function start(ext: ExtensionContext): Promise<Context> {
+    if (workspace.workspaceFolders === undefined) {
+        throw new Error('Cannot determine project folder (workspace.workspaceFolders is undefined)');
+    }
+    const projectUri = workspace.workspaceFolders[0].uri;
     const wasm = await Wasm.load();
-    const fs = await wasm.createRootFileSystem([ { kind: 'workspaceFolder' }]);
-    const pty = wasm.createPseudoterminal();
-    const terminal = window.createTerminal({ name: 'kcide', pty, isTransient: true });
+    const fs = await wasm.createRootFileSystem([ { kind: 'workspaceFolder' } ]);
     const diagnostics = languages.createDiagnosticCollection('kcide');
-    const bits = await workspace.fs.readFile(Uri.joinPath(context.extensionUri, 'media/asmx.wasm'));
-    const module = await WebAssembly.compile(bits);
-    return { wasm, fs, pty, terminal, diagnostics, module };
+    const bits = await workspace.fs.readFile(Uri.joinPath(ext.extensionUri, 'media/asmx.wasm'));
+    const asmx = await WebAssembly.compile(bits);
+    return { projectUri, wasm, fs, diagnostics, asmx };
 }
 
-export async function run(_context: ExtensionContext, state: State, args: string[]) {
+export async function runAsmx(ctx: Context, project: Project, args: string[]): Promise<RunResult> {
+    const stdoutUri = getOutputFileUri(ctx, project, 'stdout.txt');
+    const stderrUri = getOutputFileUri(ctx, project, 'stderr.txt');
+    try { await workspace.fs.delete(stdoutUri); } catch (err) {};
+    try { await workspace.fs.delete(stderrUri); } catch (err) {};
+    const [ stdoutWasmPath, stderrWasmPath ] = await Promise.all([
+        uriToWasmPath(ctx, stdoutUri),
+        uriToWasmPath(ctx, stderrUri)
+    ]);
+    const process = await ctx.wasm.createProcess('asmx', ctx.asmx, {
+        rootFileSystem: ctx.fs,
+        stdio: {
+            out: { kind: 'file', path: stdoutWasmPath },
+            err: { kind: 'file', path: stderrWasmPath },
+        },
+        args,
+    });
+    const exitCode = await process.run();
+    const result: RunResult = { exitCode };
     try {
-        state.terminal.show(true);
-        const process = await state.wasm.createProcess('asmx', state.module, {
-            rootFileSystem: state.fs,
-            stdio: state.pty.stdio,
-            args,
-        });
-        const exitCode = await process.run();
+        result.stdout = fs.readFileSync(stdoutUri.fsPath, { encoding: 'utf8' });
     } catch (err) {
-        window.showErrorMessage((err as Error).message);
-    }
+        console.warn(`runAsmx(): failed to read ${stdoutUri.fsPath }`);
+    };
+    try {
+        result.stderr = fs.readFileSync(stderrUri.fsPath, { encoding: 'utf8' });
+    } catch (err) {
+        console.warn(`runAsmx(): failed to read ${stderrUri.fsPath }`);
+    };
+    return result;
 }
 
-export async function assemble(context: ExtensionContext, state: State) {
-    console.log('Assemble called!');
-    state.diagnostics.clear();
-    const src = await findMainSource();
-    if (src === undefined) {
-        window.showErrorMessage('No project main file found (must be "src/main.asm")');
-        return;
+function parseDiagnostics(ctx: Context, stderr: string | undefined): ReadonlyArray<[Uri, readonly Diagnostic[] | undefined]> {
+    if (stderr === undefined) {
+        return [];
     }
-    console.log(`assembling: ${src}`);
-    await run(context, state, [ '-l', '-o', '-w', '-e', '-C', 'z80', '/workspace/src/main.asm' ]);
-    console.log('done.');
+    const pathPrefix = ctx.projectUri.fsPath;
+    return stderr.split('\n').filter((line) => line.includes('*** Error:') || line.includes('*** Warning:')).map((item) => {
+        const parts = item.split(':');
+        const srcPath = parts[0].replace('/workspace', pathPrefix);
+        const lineNr = Number(parts[1]) - 1;
+        const type = parts[2];
+        const msg = parts[3].slice(2, -4);
+        const severity = type === ' *** Error' ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning;
+        return [Uri.file(srcPath), [new Diagnostic(new Range(lineNr, 0, lineNr, 256), msg, severity)]];
+    });
+}
 
-    const uri = (await workspace.findFiles('src/main.asm'))[0];
-    state.diagnostics.set(uri, [ new Diagnostic(new Range(2, 0, 2, 80), 'Bla bla bla', DiagnosticSeverity.Error )]);
+export async function assemble(_ext: ExtensionContext, ctx: Context) {
+    console.log('=> assemble:');
+    try {
+        ctx.diagnostics.clear();
+        const project = loadProject(ctx);
+        console.log(`=> assemble: project file loaded: ${JSON.stringify(project, null, 2)}`);
+        const mainSrcUri = getMainSourceUri(ctx, project);
+        if (mainSrcUri === undefined) {
+            window.showErrorMessage(`Project main file '${project.mainFile}' not found!`);
+            return;
+        }
+        const [ srcPath, objPath, lstPath ] = await Promise.all([
+            uriToWasmPath(ctx, mainSrcUri),
+            uriToWasmPath(ctx, getOutputObjectFileUri(ctx, project)),
+            uriToWasmPath(ctx, getOutputListFileUri(ctx, project)),
+        ]);
+        const args = [ '-l', lstPath, '-o', objPath, '-w', '-e', '-C', project.cpu, srcPath ];
+        console.log(`=> assemble: asmx args: ${args.join(' ')}`);
+        const result = await runAsmx(ctx, project, args);
+        console.log(`=> assemble: asmx result: ${JSON.stringify(result, null, 2)}`);
+        ctx.diagnostics.set(parseDiagnostics(ctx, result.stderr));
+    } catch (err) {
+        window.showErrorMessage(`assemble() failed with ${(err as Error).message}`);
+    }
 }
