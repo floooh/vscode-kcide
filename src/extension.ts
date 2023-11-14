16d376209512b14dba9f14b1ce646948ff2f3991
Andre Weissflog <floooh@gmail.com>
2023-11-14 22:19:29 +0100

    tabs to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -3,21 +3,21 @@ import { start, build, check } from './kcide';
 
 export async function activate(ext: vscode.ExtensionContext) {
 
-	console.log('vscode-kcide: activate() called');
-	try {
-		const ctx = await start(ext);
-		console.log('vscode-kcide: started');
+    console.log('vscode-kcide: activate() called');
+    try {
+        const ctx = await start(ext);
+        console.log('vscode-kcide: started');
 
-		const cmdBuild = vscode.commands.registerCommand('floooh.kcide.build', async() => {
-			await build(ctx);
-		});
-		const cmdCheck = vscode.commands.registerCommand('floooh.kcide.check', async() => {
-			await check(ctx);
-		});
-		ext.subscriptions.push(cmdBuild, cmdCheck);
-	} catch (err) {
-		vscode.window.showErrorMessage(`Failed to setup KCIDE extension (${(err as Error).message}`);
-	}
+        const cmdBuild = vscode.commands.registerCommand('floooh.kcide.build', async() => {
+            await build(ctx);
+        });
+        const cmdCheck = vscode.commands.registerCommand('floooh.kcide.check', async() => {
+            await check(ctx);
+        });
+        ext.subscriptions.push(cmdBuild, cmdCheck);
+    } catch (err) {
+        vscode.window.showErrorMessage((err as Error).message);
+    }
 }
 
 // This method is called when your extension is deactivated
