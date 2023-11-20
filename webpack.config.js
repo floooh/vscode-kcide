72311d86755a5fb12f2fd0f9a3af00c3a83a5fa6
Andre Weissflog <floooh@gmail.com>
2023-11-20 18:57:53 +0100

    try adding back browser support

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ const webExtensionConfig = {
       // Webpack 5 no longer polyfills Node.js core modules automatically.
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
-      assert: require.resolve('assert')
+      assert: require.resolve('assert'),
+      url: require.resolve('url'),
+      process: require.resolve('process/browser')
     }
   },
   module: {
