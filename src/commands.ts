dc6df61d3e8e4d6ad28082dbe1bdb8ed45abb583
Andre Weissflog <floooh@gmail.com>
2023-12-03 17:16:02 +0100

    make build errors more obvious

--- src/commands.ts
+++ src/commands.ts
@@ -11,9 +11,11 @@ export async function asmBuild(ext: ExtensionContext) {
         const project = await loadProject();
         const result = await assemble(ext, project, { genListingFile: true, genObjectFile: true, genMapFile: true });
         const diagnostics = updateDiagnosticsFromStderr(project.uri, result.stderr);
-        if (diagnostics.numErrors=== 0) {
+        if (diagnostics.numErrors === 0) {
             const uri = await writeOutputFile(project, result.objectUri!, true);
             window.showInformationMessage(`Output written to ${uri.path}`);
+        } else {
+            window.showErrorMessage(`Build failed with ${diagnostics.numErrors} error(s)`);
         }
     } catch (err) {
         window.showErrorMessage((err as Error).message);
